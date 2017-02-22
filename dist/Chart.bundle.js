e77df7cb63fdf713e74b64a01c407e1abd651b2f
Adam Kirk <atomkirk@gmail.com>
2017-02-22 10:22:16 -0700

    even better bar charts

--- dist/Chart.bundle.js
+++ dist/Chart.bundle.js
@@ -14740,10 +14740,9 @@ module.exports = function(Chart) {
 				        ctx.moveTo(x + radius, y);
 				        ctx.lineTo(x + width - radius, y);
 				        ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
-				        ctx.lineTo(x + width, y + height - radius);
-				        ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
-				        ctx.lineTo(x + radius, y + height);
-				        ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+				        ctx.lineTo(x + width, y + height);
+				        ctx.lineTo(x, y + height);
+				        ctx.quadraticCurveTo(x, y + height, x, y + height);
 				        ctx.lineTo(x, y + radius);
 				        ctx.quadraticCurveTo(x, y, x + radius, y);
 			        }
@@ -14751,10 +14750,8 @@ module.exports = function(Chart) {
 				        ctx.moveTo(x + radius, y);
 				        ctx.lineTo(x + width - radius, y);
 				        ctx.quadraticCurveTo(x + width, y, x + width, y - radius);
-				        ctx.lineTo(x + width, y - height + radius);
-				        ctx.quadraticCurveTo(x + width, y - height, x + width - radius, y - height);
-				        ctx.lineTo(x + radius, y - height);
-				        ctx.quadraticCurveTo(x, y - height, x, y - height + radius);
+				        ctx.lineTo(x + width, y - height);
+				        ctx.lineTo(x, y - height);
 				        ctx.lineTo(x, y - radius);
 				        ctx.quadraticCurveTo(x, y, x + radius, y);
 			        }
--- src/elements/element.rectangle.js
+++ src/elements/element.rectangle.js
@@ -151,10 +151,8 @@ module.exports = function(Chart) {
 				        ctx.moveTo(x + radius, y);
 				        ctx.lineTo(x + width - radius, y);
 				        ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
-				        ctx.lineTo(x + width, y + height - radius);
-				        ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
-				        ctx.lineTo(x + radius, y + height);
-				        ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+				        ctx.lineTo(x + width, y + height);
+				        ctx.lineTo(x, y + height);
 				        ctx.lineTo(x, y + radius);
 				        ctx.quadraticCurveTo(x, y, x + radius, y);
 			        }
@@ -162,10 +160,8 @@ module.exports = function(Chart) {
 				        ctx.moveTo(x + radius, y);
 				        ctx.lineTo(x + width - radius, y);
 				        ctx.quadraticCurveTo(x + width, y, x + width, y - radius);
-				        ctx.lineTo(x + width, y - height + radius);
-				        ctx.quadraticCurveTo(x + width, y - height, x + width - radius, y - height);
-				        ctx.lineTo(x + radius, y - height);
-				        ctx.quadraticCurveTo(x, y - height, x, y - height + radius);
+				        ctx.lineTo(x + width, y - height);
+				        ctx.lineTo(x, y - height);
 				        ctx.lineTo(x, y - radius);
 				        ctx.quadraticCurveTo(x, y, x + radius, y);
 			        }
